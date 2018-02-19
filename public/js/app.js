b85d97063c1b967aa8d7eaa66c18704bc25422a8
AraceliGS <aragutarras@gmail.com>
2018-02-19 11:30:59 -0500

    añadiendo evento click a los botones- identificando cada botón donde se aplica el evento

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,25 @@
-$(document).on('ready', () => {
+$(document).ready(() => {
   $('#modal1').modal();
-  $('#modal1').modal();
+  // Variables
+  const $postsContainer = $('#posts-container');
+  $postsContainer.append('<row class="col s12 m12 l12"/>');
+  const $buttonContainer = $('#button-container');
+  const $postButton = $('#post-button');
+  const $imageButton = $('#file-button');
+  const $calendarButton = $('#calendar-button');
+  const $videoButton = $('#video-button');
+
+  $buttonContainer.on('click', '.modal-trigger', (event) => {
+    if (event.currentTarget === $postButton[0]) {
+      console.log('Este botón te permite postear mensajes');
+    } else if (event.currentTarget === $imageButton[0]) {
+      console.log('Este botón te permite postear imágenes');
+    } else if (event.currentTarget === $calendarButton[0]) {
+      console.log('Este botón te permite postear la fecha actual y tu ubicación');
+    } else if (event.currentTarget === $videoButton[0]) {
+      console.log('Este botón te permite postear audios y vídeos');
+    } else {
+      console.log('Error. Por favor, inténtelo nuevamente.');
+    }
+  });
 });
