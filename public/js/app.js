74452f14de5b8335c2ee0c06a35f65def07d96c6
AraceliGS <aragutarras@gmail.com>
2018-02-19 12:17:28 -0500

    añadiendo avances en la funcionalidad, definiendo estructura del modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,24 @@
 $(document).ready(() => {
   $('#modal1').modal();
-  // Variables
+  // Variable que referencia al contendor de los posts
   const $postsContainer = $('#posts-container');
   $postsContainer.append('<row class="col s12 m12 l12"/>');
+  // Variables que referencian a los botones y su contenedor
   const $buttonContainer = $('#button-container');
   const $postButton = $('#post-button');
   const $imageButton = $('#file-button');
   const $calendarButton = $('#calendar-button');
   const $videoButton = $('#video-button');
+  // Variables que referencian a los elementos del modal
+  const $modalContent = $('#modal-body');
 
   $buttonContainer.on('click', '.modal-trigger', (event) => {
     if (event.currentTarget === $postButton[0]) {
       console.log('Este botón te permite postear mensajes');
     } else if (event.currentTarget === $imageButton[0]) {
       console.log('Este botón te permite postear imágenes');
+      $modalContent.find('');
+      //
     } else if (event.currentTarget === $calendarButton[0]) {
       console.log('Este botón te permite postear la fecha actual y tu ubicación');
     } else if (event.currentTarget === $videoButton[0]) {
