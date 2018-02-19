0136cc2121bdc404913589a7ba5cb4b98cc7df0d
AraceliGS <aragutarras@gmail.com>
2018-02-19 17:44:50 -0500

    añadiendo avances en la funcionalidad

--- public/js/app.js
+++ public/js/app.js
@@ -15,12 +15,20 @@ $(document).ready(() => {
   const $modalContent = $('#modal-body');
 
   $buttonContainer.on('click', '.modal-trigger', (event) => {
+    // Modal para el botón que publica posts
     if (event.currentTarget === $postButton[0]) {
+      $modalContent.find('#title-container .input-field').empty();
+      $modalContent.find('#add-post-container .input-field').empty();
       $('<input placeholder="Placeholder" id="first_name" type="text" class="validate">').appendTo($placeMessage);
       $('<label class="active" for="first_name" id="title-label"></label>').appendTo($placeMessage);
       $('#title-container .input-field').find('#title-label').text('Título');
+      //
       console.log('Este botón te permite postear mensajes');
+      // Insertar input para los mensajes
+      // Modal para el botón que publica imágenes
     } else if (event.currentTarget === $imageButton[0]) {
+      $modalContent.find('#title-container .input-field').empty();
+      $modalContent.find('#add-post-container .input-field').empty();
       console.log('Este botón te permite postear imágenes');
       $modalContent.find('#title-container .input-field').append('<input placeholder="Placeholder" id="first_name" type="text" class="validate">');
       $modalContent.find('#title-container .input-field').append('<label class="active" for="first_name" id="title-label"></label>');
@@ -28,10 +36,18 @@ $(document).ready(() => {
       //
       $modalContent.find('#add-post-container .input-field').append('<div class="btn"><span>Imagen</span><input type="file"></div>');
       $modalContent.find('#add-post-container .input-field').append('<div class="file-path-wrapper"><input class="file-path validate" type="text"></div>');
+      // Modal para el botón que publica la fecha y ubicación del usuario.
     } else if (event.currentTarget === $calendarButton[0]) {
       console.log('Este botón te permite postear la fecha actual y tu ubicación');
+      // 
+      $modalContent.find('#title-container .input-field').empty();
+      $modalContent.find('#add-post-container .input-field').empty();
+      // Modal para el botón que publica vídeos y audios.
     } else if (event.currentTarget === $videoButton[0]) {
       console.log('Este botón te permite postear audios y vídeos');
+      // 
+      $modalContent.find('#title-container .input-field').empty();
+      $modalContent.find('#add-post-container .input-field').empty();
     } else {
       console.log('Error. Por favor, inténtelo nuevamente.');
     }
