af7da11741f7825a13138efd596c36a90ea20b5d
LeslyN <lnombertoc@gmail.com>
2018-02-20 12:56:52 -0500

    video terminado

--- public/js/app.js
+++ public/js/app.js
@@ -228,9 +228,9 @@ $(document).ready(() => {
         }); 
       });
       //
-      $modalContent.find('#add-post-container .input-field').append('<div class="btn"><span>VIDEO O AUDIO</span><input type="file" id="files" name="files[]" multiple><output id="list"></output></div>');
+      $modalContent.find('#add-post-container .input-field').append('<div class="btn"><span>VIDEO O AUDIO</span><input type="file" id="files" name="files[]" multiple><output id="list-video"></output></div>');
       $modalContent.find('#add-post-container .input-field').append('<div class="file-path-wrapper"><input class="file-path validate" type="text"></div>');
-      $modalContent.find('#add-post-container .input-field').append('<video controls><source src=""></video>');
+      $modalContent.find('#add-post-container .input-field').append('<video controls><source src="" type="video/mp4/mp3"></video>');
     
       if (window.File && window.FileReader && window.FileList && window.Blob) {
         // Great success! All the File APIs are supported.
@@ -251,10 +251,12 @@ $(document).ready(() => {
             reader.onload = (function(theFile) {
               return function(e) {
                 // Render thumbnail.
+                debugger;
                 var span = document.createElement('span');
-                span.innerHTML = ['<source class="thumb" src="', e.target.result,
-                                  '" title="', escape(theFile.name), '"/>'].join('');
-                document.getElementById('list').insertBefore(span, null);
+                
+                span.innerHTML = ['<video class="thumb" controls><source src="', e.target.result,
+                                  '" title="', escape(theFile.name), '"/></video>'].join('');
+                document.getElementById('list-video').insertBefore(span, null);
               };
             })(f);
       
